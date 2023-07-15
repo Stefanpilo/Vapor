23d48502064d5a46c51768ded1ed2cdcbae436fc
Stefanpilo <stefanpilo@yahoo.com>
2023-07-15 16:19:29 +0200

    /

--- Vapor/src/main/java/model/Videogioco.java
+++ Vapor/src/main/java/model/Videogioco.java
@@ -1,20 +1,74 @@
 package model;
 
 public class Videogioco {
-	public Videogioco(int aid, String img, String tit, float prez, float sco, String desc, String cat) {
-		ID = aid;
-		immagine = img;
-		titolo = tit;
-		prezzo = prez;
-		sconto = sco;
-		descrizione = desc;
-		categoria = cat;
+	public Videogioco(int ID, String immagine, String titolo, float prezzo, float sconto, String descrizione, String categoria) {
+		this.ID = ID;
+		this.immagine = immagine;
+		this.titolo = titolo;
+		this.prezzo = prezzo;
+		this.sconto = sconto;
+		this.descrizione = descrizione;
+		this.categoria = categoria;
+	}
+	
+	//cia
+	
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+	
+	public int getID() {
+		return ID;
+	}
+	
+	public void setImmagine(String immagine) {
+		this.immagine = immagine;
+	}
+	
+	public String getImmagine() {
+		return immagine;
+	}
+	
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
 	}
 	
 	public String getTitolo() {
 		return titolo;
 	}
-
+	
+	public void setPrezzo(float prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	public float getPrezzo() {
+		return prezzo;
+	}
+	
+	public void setSconto(float sconto) {
+		this.sconto = sconto;
+	}
+	
+	public float getSconto() {
+		return sconto;
+	}
+	
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+	public String getDescrizione() {
+		return descrizione;
+	}
+	
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	public String getCategoria() {
+		return categoria;
+	}
+	
 	private int ID;
 	private String immagine;
 	private String titolo;
