446250e15e40f4af5e6587ab24bd24a44034e7e9
Stefanpilo <stefanpilo@yahoo.com>
2023-07-22 02:19:22 +0200

    Carrello Page + Carrello servlet - Aggiunta la possibilità di aggiungere ordini al carrello e di visualizzarli

---
+++ Vapor/src/main/webapp/scripts/carrelloPage_script.js
@@ -0,0 +1,6 @@
+function loadCarrelloScript() {
+	document.querySelectorAll(".deleteRow_button").forEach( (element) => {
+		//console.log(element);
+	});
+};
+document.addEventListener('DOMContentLoaded', loadCarrelloScript);
